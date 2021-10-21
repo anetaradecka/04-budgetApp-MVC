e7a8848b0362c997d7ecd2c05e078a3cff3b7370
Aneta Radecka <aneta.radecka@outlook.com>
2021-10-21 20:38:16 +0200

    load balance automatically

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -9,14 +9,18 @@ class Balance extends Authenticated
 {
     public function indexAction()
     {
+        $total_revenues = Budget::getTotalRevenues();
+        $total_expenses = Budget::getTotalExpenses();
+
         View::renderTemplate('Balance/index.html', [
             'period' => Budget::getPeriod(),
             'start_date' => Budget::getStartDate(),
             'end_date' => Budget::getEndDate(),
             'revenues' => Budget::getRevenues(),
-            'total_revenues' => Budget::getTotalRevenues(),
+            'total_revenues' => $total_revenues,
             'expenses' => Budget::getExpenses(),
-            'total_expenses' => Budget::getTotalExpenses()
+            'total_expenses' => $total_expenses,
+            'total' => Budget::calculate($total_revenues, $total_expenses)
         ]);
     }
 }
--- App/Models/Budget.php
+++ App/Models/Budget.php
@@ -41,10 +41,12 @@ class Budget extends \Core\Model
             $sql = 'SELECT revenue_category_assigned_to_user, amount, revenue_date AS date, comment, name
                     FROM revenues LEFT JOIN revenue_category_default ON revenues.revenue_category_assigned_to_user = revenue_category_default.id
                     WHERE user_id = :user_id AND revenue_date BETWEEN :start_date AND :end_date';
+            $start_date = static::getStartDate();
+            $end_date = static::getEndDate();
             $statement = $db->prepare($sql);
             $statement->bindParam(':user_id', $user->id, PDO::PARAM_INT);
-            $statement->bindParam(':start_date', $_POST['start_date'], PDO::PARAM_STR);
-            $statement->bindParam(':end_date', $_POST['end_date'], PDO::PARAM_STR);
+            $statement->bindParam(':start_date', $start_date, PDO::PARAM_STR);
+            $statement->bindParam(':end_date', $end_date, PDO::PARAM_STR);
             $statement->execute();
             return $statement->fetchAll(PDO::FETCH_ASSOC);
         }
@@ -58,10 +60,12 @@ class Budget extends \Core\Model
         if ($user) {
             $db = static::getDB();
             $sql = 'SELECT SUM(amount) as total FROM revenues WHERE user_id = :user_id AND revenue_date BETWEEN :start_date AND :end_date';
+            $start_date = static::getStartDate();
+            $end_date = static::getEndDate();
             $statement = $db->prepare($sql);
             $statement->bindParam(':user_id', $user->id, PDO::PARAM_INT);
-            $statement->bindParam(':start_date', $_POST['start_date'], PDO::PARAM_STR);
-            $statement->bindParam(':end_date', $_POST['end_date'], PDO::PARAM_STR);
+            $statement->bindParam(':start_date', $start_date, PDO::PARAM_STR);
+            $statement->bindParam(':end_date', $end_date, PDO::PARAM_STR);
             $statement->execute();
             return $statement->fetch(PDO::FETCH_ASSOC)['total'];
         }
@@ -77,10 +81,12 @@ class Budget extends \Core\Model
             $sql = 'SELECT expense_category_assigned_to_user, amount, expense_date AS date, comment, name
                     FROM expenses LEFT JOIN expense_category_default ON expenses.expense_category_assigned_to_user = expense_category_default.id
                     WHERE user_id = :user_id AND expense_date BETWEEN :start_date AND :end_date';
+            $start_date = static::getStartDate();
+            $end_date = static::getEndDate();
             $statement = $db->prepare($sql);
             $statement->bindParam(':user_id', $user->id, PDO::PARAM_INT);
-            $statement->bindParam(':start_date', $_POST['start_date'], PDO::PARAM_STR);
-            $statement->bindParam(':end_date', $_POST['end_date'], PDO::PARAM_STR);
+            $statement->bindParam(':start_date', $start_date, PDO::PARAM_STR);
+            $statement->bindParam(':end_date', $end_date, PDO::PARAM_STR);
             $statement->execute();
             return $statement->fetchAll(PDO::FETCH_ASSOC);
         }
@@ -94,10 +100,12 @@ class Budget extends \Core\Model
         if ($user) {
             $db = static::getDB();
             $sql = 'SELECT SUM(amount) as total FROM expenses WHERE user_id = :user_id AND expense_date BETWEEN :start_date AND :end_date';
+            $start_date = static::getStartDate();
+            $end_date = static::getEndDate();
             $statement = $db->prepare($sql);
             $statement->bindParam(':user_id', $user->id, PDO::PARAM_INT);
-            $statement->bindParam(':start_date', $_POST['start_date'], PDO::PARAM_STR);
-            $statement->bindParam(':end_date', $_POST['end_date'], PDO::PARAM_STR);
+            $statement->bindParam(':start_date', $start_date, PDO::PARAM_STR);
+            $statement->bindParam(':end_date', $end_date, PDO::PARAM_STR);
             $statement->execute();
             return $statement->fetch(PDO::FETCH_ASSOC)['total'];
         }
@@ -186,5 +194,9 @@ class Budget extends \Core\Model
         }
     
         return false;
+    }
+
+    public static function calculate($value1, $value2) {
+        return static::toDecimal($value1 - $value2);
     }
 }
